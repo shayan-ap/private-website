1b04949753e34daf1d34faa7816c0c451c4073ec
yasin <yasinchangaei79@gmail.com>
2024-02-26 11:00:10 +0330

    start product page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/home/home";
 import About from "./pages/about us/aboutUs";
 import Contact from "./pages/contact us/contactUs";
+import Product from "./pages/product/Product";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/product" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/product/Product.js
@@ -0,0 +1,12 @@
+import MyNavbar from "../../Components/Navbar/Navbar";
+
+function Product() {
+  return (
+    <>
+      <MyNavbar />
+      <h1>shayan</h1>
+    </>
+  );
+}
+
+export default Product;
